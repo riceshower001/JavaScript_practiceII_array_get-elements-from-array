f188806c99a15402aaf1c66ca2bbb50bc106a375
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-27 02:30:07 +0900

    Add files via upload

---
+++ JavaScript_practiceII_array_get elements from array.js
@@ -0,0 +1,9 @@
+// Let's extract elements from an array and print it.
+
+const animals = ["dog", "cat", "sheep"];
+
+// Please print the first element of the array.
+console.log(animals[0]);
+
+// Please print the third element of the array.
+console.log(animals[2]);
